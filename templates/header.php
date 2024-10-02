c0a488b4ac495f3af77e21f84b90bc4410984d08
mlsfront <mlsfront@outlook.com>
2024-10-02 20:23:28 -0300

    feat: Ativar link no menu de acordo com a URL --modified:   templates/header.php

--- templates/header.php
+++ templates/header.php
@@ -9,19 +9,39 @@ require_once('../src/config.php');
     <title>Sistema de Login</title>
     <link rel="stylesheet" href="<?php echo URL_ASSETS; ?>css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+
+        .nav-link.active {
+            font-weight: bold;
+        }
+    </style>
 </head>
-<body>
-    <header>
+<body class="bg-light">
+    <header class="bg-dark text-white p-3">
         <h1>Projeto Sistema de Login</h1>
         <nav>
-            <ul>
-                <li><a href="<?php echo URL_PUBLIC;?>index.php">Home</a></li>
-                <li><a href="<?php echo URL_PUBLIC;?>login.php">Login</a></li>
-                <li><a href="<?php echo URL_PUBLIC;?>register.php">Registrar</a></li>
-                <li><a href="<?php echo URL_PUBLIC;?>reset_password.php">Recuperar Senha</a></li>
-                <li><a href="<?php echo URL_PUBLIC;?>logout.php ">Sair</a></li>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>" href="<?php echo URL_PUBLIC;?>index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white <?php echo basename($_SERVER['PHP_SELF']) == 'login.php' ? 'active' : ''; ?>" href="<?php echo URL_PUBLIC;?>login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white <?php echo basename($_SERVER['PHP_SELF']) == 'register.php' ? 'active' : ''; ?>" href="<?php echo URL_PUBLIC;?>register.php">Registrar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white <?php echo basename($_SERVER['PHP_SELF']) == 'reset_password.php' ? 'active' : ''; ?>" href="<?php echo URL_PUBLIC;?>reset_password.php">Recuperar Senha</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light disabled" aria-disabled="true">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="<?php echo URL_PUBLIC;?>logout.php">Sair</a>
+                </li>
             </ul>
         </nav>
+
     </header>
 
-    <div class="container-fluid">
+    <div class="py-4">
